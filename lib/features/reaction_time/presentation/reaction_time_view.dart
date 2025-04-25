73899d47acf5554d7d7e769b4504058a4bce394e
Ammar Ayman <168241329+AmmarAyman108@users.noreply.github.com>
2025-04-25 23:24:25 +0300

    feat: Add ReactionTimeView component with basic layout

---
+++ lib/features/reaction_time/presentation/reaction_time_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ReactionTimeView extends StatelessWidget {
+  const ReactionTimeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reaction Time'),
+      ),
+      body: const Center(
+        child: Text(
+          'This is the Reaction Time View',
+        ),
+      ),
+    );
+  }
+}
